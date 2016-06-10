2530661fb2aae43064c89ffe08a46d2a196089e8
Julia Dickey <julia.dickey@gmail.com>
2016-06-10 09:39:45 -0700

    Create first perameter for output, swapping multiples of both 3 and 5 for Pingpong

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 
 // BUSINESS LOGIC
-//
-// var converter = function(number){
-// for( i=0 ; i<number ; i++ ){
-//   if ( number % 3 === 0 && number % 5 === 0 ){
-//     var newNumber = "PingPong";
-//   } else {
-//     alert("Test");
-//     }
-//   }
-//   return newNumber;
-// }
+
+var converter = function(number){
+for( i = 0 ; i < number ; i++ ) {
+  if ( number % 3 === 0 && number % 5 === 0 ){
+    var newNumber = "PingPong";
+  }
+  else {
+    var newNumber = 4;
+  }
+  } return newNumber;
+}
 
 
 // USER LOGIC
@@ -19,9 +19,9 @@ $(document).ready(function() {
   $("form#game").submit(function(event){
     event.preventDefault();
     var enteredNumber = parseInt($("#userInput").val());
-    // var output = converter(userInput);
+    var output = converter(enteredNumber);
 
-    $("#results").append("<li>" + enteredNumber + "</li>");
+    $("#results").append("<li>" + output + "</li>");
     $("#answer").show();
   });
 });

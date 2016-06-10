b843290db925550c446f2f475f98e8f050c71795
Julia Dickey <julia.dickey@gmail.com>
2016-06-10 14:18:31 -0700

    clears previous result on new submit click

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,11 @@
-
 // BUSINESS LOGIC
-var numberList = [];
 var pingPongArray = [];
-
 
 // TURNS INPUT NUMBER INTO AN ARRAY
 var nowArray = function(num){
-  if (!isNaN(num)){
-    // var numberArray = num.map(Number);
-    for ( k=1 ; k <= num ; k++ ){
+  numberList = [];
+  if (!isNaN(num)) {
+    for ( k = 1 ; k <= num ; k++ ){
     numberList.push(k);
     }
     converter(numberList);
@@ -18,9 +15,9 @@ var nowArray = function(num){
 }
 // CALL CONVERTER FUNCTION TO TURN NUMBERLIST INTO PING PONG LIST
 
-
 // TRANSLATES EVERY ITEM IN THE ARRAY TO PING PONG TERMS
 var converter = function(numberArray){
+  pingPongArray = [];
   for( var i = 1 ; i <= numberArray.length ; i++ ) {
     if ((numberArray[i-1] % 3 === 0) && ((numberArray[i-1] % 5) === 0 )) {
       pingPongArray.push("pingpong");
@@ -37,21 +34,17 @@ var converter = function(numberArray){
     }
 }
 
-
 // USER LOGIC
 $(document).ready(function() {
 
   $("form#game").submit(function(event){
+    $("#results").empty();
     event.preventDefault();
     var enteredNumber = parseInt($("#userInput").val());
-    // var output = converter(enteredNumber);
-
     nowArray(enteredNumber);
-
-    for ( var j=0 ; j < pingPongArray.length; j++) {
-      $("#results").append("<li>" + pingPongArray[j] + "</li>");
-    };
-    pingPongArray=[];
+      for ( var j=0 ; j < pingPongArray.length; j++) {
+        $("#results").append("<li>" + pingPongArray[j] + "</li>");
+      };
     $("#answer").show();
   });
 });
